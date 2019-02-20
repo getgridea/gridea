b9747eeae326f4f513507e41190d2b72e5296b3a
EryouHao <haoeryou@qq.com>
2019-02-20 15:55:36 +0800

    refactor: add nsis

--- vue.config.js
+++ vue.config.js
@@ -23,6 +23,14 @@ module.exports = {
           icon: './public/app-icons/logo-fang2.icns',
         },
         asar: false,
+        nsis: {
+          oneClick: false, // 是否一键安装
+          allowElevation: true, // 允许请求提升。 如果为false，则用户必须使用提升的权限重新启动安装程序。
+          allowToChangeInstallationDirectory: true, // 允许修改安装目录
+          createDesktopShortcut: true, // 创建桌面图标
+          createStartMenuShortcut: true,// 创建开始菜单图标
+          shortcutName: 'Hve Notes', // 图标名称
+        },
       },
     },
   },

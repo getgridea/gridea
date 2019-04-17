03c03b1445038c2e5a1716d6242b64334e1714aa
steve xu <stevexugc@gmail.com>
2019-04-17 09:54:16 +0800

    update ts types file

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,7 +1,7 @@
-import { AllElectron } from "electron"
+import { AllElectron } from 'electron'
 import * as dayjs from 'dayjs'
 
-declare module "vue/types/vue" {
+declare module 'vue/types/vue' {
   interface Vue {
     readonly $electron: AllElectron,
     $dayjs: any,

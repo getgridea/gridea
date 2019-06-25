3e3ae74491d0271c188087015167efc74969e5a4
EryouHao <haoeryou@qq.com>
2019-06-26 00:06:18 +0800

    update: add sql code view style

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
   ],
   plugins: [
     ['prismjs', {
-      'languages': ['javascript', 'css', 'markup', 'json', 'bash', 'sass', 'python', 'typescript', 'java', 'less', 'php', 'pug', 'jsx', 'c', 'ruby', 'rust', 'dart', 'stylus', 'swift', 'yaml'],
+      'languages': ['javascript', 'css', 'markup', 'json', 'bash', 'sass', 'python', 'typescript', 'java', 'less', 'php', 'pug', 'jsx', 'c', 'ruby', 'rust', 'dart', 'stylus', 'swift', 'yaml', 'sql'],
       'plugins': ['line-numbers'],
       'theme': 'default',
       'css': true,
